a3d8d23336631ef40b4694cc026284a085e02ef6
Wojtek <wojtekpiec123@gmail.com>
2025-10-26 00:29:33 +0200

    moved intervl reading to new task

--- src/firebase.c
+++ src/firebase.c
@@ -5,15 +5,20 @@
 
 #include "firebase.h"
 
+#define MAX_RETRY_NUM 5
+#define RETRY_DELAY_MS 500
 static const char *TAG = "firebase_client";
-extern esp_err_t firebase_put(const char *path, const char *json_payload);
 
 const char *FIREBASE_BASE_URL = "https://espbackendapp-default-rtdb.europe-west1.firebasedatabase.app/";
 
-esp_err_t firebase_put(const char *path, const char *json_payload)
+static esp_err_t _firebase_put_htpp(const char *path, const char *json_payload)
 {
+    int retry_cnt =0;
     char url[256];
+     esp_err_t err = ESP_FAIL;
     snprintf(url, sizeof(url), "%s/%s.json", FIREBASE_BASE_URL, path); 
+
+    do{
 
     esp_http_client_config_t config = {
         .url = url,
@@ -24,14 +29,61 @@ esp_err_t firebase_put(const char *path, const char *json_payload)
     esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_http_client_set_header(client, "Content-Type", "application/json");
     esp_http_client_set_post_field(client, json_payload, strlen(json_payload));
-    esp_err_t err = esp_http_client_perform(client);
+   err = esp_http_client_perform(client);
     if (err == ESP_OK) {
-        ESP_LOGI(TAG, "PUT status=%d", esp_http_client_get_status_code(client));
+        int status_code = esp_http_client_get_status_code(client);
+
+        if(status_code >=200 && status_code < 300){
+            ESP_LOGI(TAG, "PUT Success status=%d", status_code);
+            err = ESP_OK;
+        } else {
+            ESP_LOGW(TAG, "PUT failed (HTPP status %d)", status_code);
+            err = ESP_FAIL;
+        }        
     } else {
-        ESP_LOGE(TAG, "PUT failed: %s", esp_err_to_name(err));
+        ESP_LOGE(TAG, "PUT failed (transport): %s", esp_err_to_name(err));
     }
     esp_http_client_cleanup(client);
+
+    if(err != ESP_OK && retry_cnt < MAX_RETRY_NUM){
+        vTaskDelay(pdMS_TO_TICKS(RETRY_DELAY_MS));
+    }
+
+    retry_cnt++;
+    }   while (err != ESP_OK && retry_cnt < MAX_RETRY_NUM);
+
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "PUT FAILED after %d attempts.", MAX_RETRY_NUM);
+    }
+    
     return err;
+}
+
+esp_err_t firebase_put_float_impl(const char *path, float value){
+    char payload_str[32];
+    snprintf(payload_str, sizeof(payload_str), "%.2f", value);
+
+    return _firebase_put_htpp(path, payload_str);
+}
+
+esp_err_t firebase_put_int_impl(const char *path, int value){
+    char payload_str[32];
+    snprintf(payload_str, sizeof(payload_str), "%d", value);
+
+    return _firebase_put_htpp(path, payload_str);
+}
+
+esp_err_t firebase_put_bool_impl(const char *path, bool value){
+    char payload_str[8];
+
+    const char *bool_string = value ? "true" : "false";
+    snprintf(payload_str, sizeof(payload_str), "%s", bool_string);
+
+    return _firebase_put_htpp(path, payload_str);
+}
+
+esp_err_t firebase_put_string_impl(const char *path, const char* value){
+    return _firebase_put_htpp(path, value);
 }
 
 esp_err_t firebase_get( const char *path, char *out_buf, size_t out_len)
--- src/main.c
+++ src/main.c
@@ -11,12 +11,24 @@
 #include "dns_server.h"
 #include "wifi_provisioning.h"
 #include "firebase.h"
+#include "dht11.h"
 
 static const char* TAG = "main";
+dht11_t dht11;
+
+void dht11_fireabse_task(void){
+
+    while (true)
+    {
+        dht11_read(&dht11, 5);
+        firebase_put("DHT11/temperature",dht11.temperature);
+        firebase_put("DHT11/humidity",dht11.humidity);
+        vTaskDelay(pdMS_TO_TICKS(1000 * 60 * 5)); //5 minutes
+    }
+}
 
 void app_main(void)
 {
-
     ESP_LOGW(TAG, "Initializing NVS...");
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -25,20 +37,18 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-
-    // // DODAJ TEN FRAGMENT, ABY ZAWSZE CZYŚCIĆ PAMIĘĆ NA POTRZEBY TESTÓW
-    // ESP_LOGW(TAG, "!!! FORCING NVS ERASE TO TEST PROVISIONING !!!");
-    // ESP_ERROR_CHECK(nvs_flash_erase()); // Bezwarunkowe czyszczenie
-    // ESP_ERROR_CHECK(nvs_flash_init()); 
-    // ESP_ERROR_CHECK(ret);
+    dht11.dht11_pin = 5;
 
     // Initialize WiFi stack and start provisioning/captive portal if needed
     wifi_provisioning_start();
 
-    
+    xTaskCreate(
+        dht11_fireabse_task,
+        "DHT11_Firebase",
+        4096,
+        NULL,
+        5,
+        NULL
+    );
 
-     while (true) {
-        firebase_put("test/path", "{\"hello\":\"world\"}");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Wait for 1 second
-    }
 }
